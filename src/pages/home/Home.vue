b7962f1550b77949d5fd49993b5440a3cf694122
Misely <1291118504@qq.com>
2018-12-10 21:33:28 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './componets/Header'
+import HomeSwiper from './componets/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
